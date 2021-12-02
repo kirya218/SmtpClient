ce99fc25207aef46ca154b98308cc6d674dbb7fe
kirya218 <heart_no91@mail.ru>
2021-12-02 17:32:14 +0300

    add new files

---
+++ SMTP/Controllers/CreateMessagesController.cs
@@ -0,0 +1,24 @@
+﻿using SMTP.Models;
+using System;
+using System.IO;
+
+namespace SMTP.Controllers
+{
+    /// <summary>
+    ///     Создает письма на сообственный домен.
+    /// </summary>
+    public class CreateMessagesController
+    {
+        /// <summary>
+        ///     Создает письмо и записывает в файл.
+        /// </summary>
+        /// <param name="info">Все параметры письма</param>
+        public CreateMessagesController(ModelInfo info, string nickname)
+        {
+            string message = "<from>" + info.Message.From + "</from>" + "<subject> " + info.Message.Subject + "</subject>" + "<body>" + info.Message.Body.Replace("\r\n", string.Empty) + "</body>" + "<date>" + DateTime.Now + "</date>";
+            StreamWriter fstream = new StreamWriter("Files/Accounts/" + nickname + ".txt");
+            fstream.WriteLine(message);
+            fstream.Close();
+        }
+    }
+}
---
+++ SMTP/Entities/Users.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace SMTP.Entities
+{
+    /// <summary>
+    ///     Пользователи.
+    /// </summary>
+    public class Users
+    {
+        /// <summary>
+        ///     Все пользователи, списком.
+        /// </summary>
+        private static List<string> AllUsers = new List<string>();
+
+        /// <summary>
+        ///     Получает весь список пользователей.
+        /// </summary>
+        public Users()
+        {
+            if (AllUsers.Count == 0)
+            {
+                string[] users = Directory.GetFiles("Files/Accounts");
+                for (int i = 0; i < users.Length; i++)
+                {
+                    users[i] = users[i].Replace("Files/Accounts\\", string.Empty);
+                    users[i] = users[i].Replace(".txt", string.Empty);
+                }
+                AllUsers = users.ToList();
+            }
+        }
+
+        /// <returns>Возращает всех пользователей списком</returns>
+        public List<string> GetUsers() => AllUsers;
+    }
+}
