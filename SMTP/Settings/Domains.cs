7c6e6fad898c831a2bf30b290ec1c4811f1c6233
kirya218 <heart_no91@mail.ru>
2021-12-22 18:05:57 +0300

    Добавил нужный файл

---
+++ SMTP/Settings/Domains.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace SMTP.Settings
+{
+    public static class Domain
+    {
+        /// <summary>
+        ///     Все домены поддерживающий мой сервер.
+        /// </summary>
+        public static Dictionary<string, string[]> SettingsAllDomains = new Dictionary<string, string[]>();
+
+        /// <summary>
+        ///     Собирает всю информацию о актуальных настройках домена.
+        /// </summary>
+        static Domain()
+        {
+            string[] text = File.ReadAllLines("Files/domains.txt");
+            string domain = string.Empty, login = string.Empty, port = string.Empty;
+            foreach (var item in text)
+            {
+                if (item.Contains("domain"))
+                {
+                    string[] k = item.Split(':');
+                    domain = k[1];
+                }
+                else if (item.Contains("port"))
+                {
+                    string[] k = item.Split(':');
+                    port = k[1];
+                }
+                else if (item.Contains("login"))
+                {
+                    string[] k = item.Split(':');
+                    login = k[1];
+                }
+                else if (item.Contains("password"))
+                {
+                    string[] k = item.Split(':');
+                    string password = k[1];
+                    string[] value = { port, login, password };
+                    SettingsAllDomains.Add(domain, value);
+                }
+            }
+        }
+    }
+}
---
+++ SMTP/Settings/Settings.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using SMTP.Models;
+
+namespace SMTP.Settings
+{
+    /// <summary>
+    ///     Обрабатывает запрос настроек
+    /// </summary>
+    public static class Settings
+    {
+        /// <summary>
+        ///     Smtp-сервер, с которого производится отправление почты. Например, smtp.yandex.ru
+        /// </summary>
+        public static string Host { get; set; }
+
+        /// <summary>
+        ///     Порт, используемый smp-сервером.
+        /// </summary>
+        public static string Port { get; set; }
+
+        /// <summary>
+        ///     Открывает передачу на другие сервера.
+        /// </summary>
+        public static bool Relay { get; set; }
+
+        /// <summary>
+        ///     Домен сервера.
+        /// </summary>
+        public static string Domain { get; set; }
+
+        /// <summary>
+        ///     Собирает информацию с файла "Настройки"
+        /// </summary>
+        static Settings()
+        {
+            List<string[]> text2 = new List<string[]>();
+            string[] text = File.ReadAllLines("Files/settings.txt");
+            foreach (var item in text)
+            {
+                text2.Add(item.Split(':'));
+            }
+            Host = text2[0][1];
+            Port = text2[1][1];
+            Relay = Convert.ToBoolean(text2[2][1]);
+            Domain = text2[3][1];
+        }
+    }
+}
